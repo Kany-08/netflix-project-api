763225bc90cfea8c62b8d17d678338c05edf9045
Ghassanooooo <ghassanaldarwish@gmail.com>
2021-12-17 10:15:29 +0100

    simple demo

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get("/movies", (req, res) => {
+  res.send("Movies!");
+});
+
+app.get("/tv-shows", (req, res) => {
+  res.send("Tv Shows!");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
